0aafb968efbe9f2aa68f40f24909f08c50212bc1
CasChrist <sozurukovmaksim@gmail.com>
2024-06-09 02:05:41 +0700

    link new method with the bot body

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 from bot_token import TOKEN
 import api_requests
+import html_parses
 
 # Define start command handler
 async def start(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text(
-        "Привет! Я — бот, предоставляющий информацию о фильмах. Я беру данные с API Кинопоиска. Вот список того, что я могу делать:\n\n/movie <\"Название фильма\"> — выдам краткую информацию обо всех фильмах с таким названием (не более 10-ти)\n")
+        "Привет! Я — бот, предоставляющий информацию о фильмах. Я беру данные с API Кинопоиска. Вот список того, что я могу делать:\n\n/movie <название фильма> — выдам краткую информацию о фильме с таким названием\n\n/tickets <дата> — выдам информацию обо всех сеансах, которые идут в кинотеатрах. Если ввести команду без указания даты, выдам информацию по сеансам за текущий день.")
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
 
     start_handler = CommandHandler('start', start)
     sbn_handler = CommandHandler('movie', api_requests.search_by_name)
+    sa_handler = CommandHandler('tickets', html_parses.search_afisha)
 
     app.add_handler(start_handler)
     app.add_handler(sbn_handler)
+    app.add_handler(sa_handler)
 
     app.run_polling()
