f8f2550004e7d27665a939f6ebe96c1277293752
CasChrist <sozurukovmaksim@gmail.com>
2024-06-10 12:27:57 +0700

    change url source, rebuild keyboard interface

--- html_parses.py
+++ html_parses.py
@@ -12,7 +12,7 @@ async def search_afisha(update: Update, context: CallbackContext):
 
   date = context.args
   # URL веб-страницы с афишей кино
-  url = f'https://afisha.yandex.ru/kemerovo/cinema?date={date[0]}&period=1'
+  url = f'https://kemerovo.kinoafisha.info/movies/?date={date[0]}'
 
   # Получение содержимого веб-страницы
   response = requests.get(url)
@@ -22,29 +22,29 @@ async def search_afisha(update: Update, context: CallbackContext):
   soup = BeautifulSoup(response.text, 'html.parser')
 
   # Найти все элементы, содержащие информацию о фильмах
-  movies = soup.find_all('div', attrs={'data-component':'EventCard'})
+  movies = soup.find_all('div', class_='movieItem_info')
   # Словарь для хранения названий фильмов и ссылок на страницы с сеансами
   movies_dict = dict()
   for movie in movies:
-    movie_title = movie.find('h2').get_text()
+    movie_title = movie.find('a').get_text()
     movie_link = movie.find('a')['href']
-    index = movie_link[17:movie_link.index('?source')]
+    index = movie_link[35:-1]
     movies_dict[movie_title] = index
   
+  message = f'Кино в Кемерове на {date[0]}:\n\n'
   keyboard = []
-  for movie, link in movies_dict.items():
-    # keyboard.append([InlineKeyboardButton(movie, callback_data=link)])
-    keyboard.append([KeyboardButton(movie)])
-
-  # markup = InlineKeyboardMarkup(keyboard)
-  markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-
-  message = f'Кино в Кемерове на {date}:\n\n'
-
-  period = 1
+  period = 0
   for movie in movies_dict.keys():
-    message += str(period) + '. ' + movie + '\n'
+    message += str(period+1) + '. ' + movie + '\n'
+    if period % 6 == 0:
+      keyboard.append([InlineKeyboardButton(period+1, callback_data=movies_dict[movie])])
+    else:
+      keyboard[period // 6].append(InlineKeyboardButton(period+1, callback_data=movies_dict[movie]))
+    # keyboard.append([KeyboardButton(movie)])
     period += 1
+
+  markup = InlineKeyboardMarkup(keyboard)
+  # markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
 
   await update.message.reply_text(text=message, reply_markup=markup)
 
